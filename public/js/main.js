17a3792231bde10a0d6c78ebaf89351329aa3c70
FrancescoSTL <francescosapolizzi@gmail.com>
2015-11-14 14:22:07 -0500

    File upload to autotool.

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,19 @@ $(document).ready(function() {
 
   // Place JavaScript code here...
 
+ 	$('input[type=file]').change(function(e){
+ 		alert("here");
+ 		$("#filesUploaded").innerHTML = "";
+ 		$("#filesUploaded").hidden = false;
+ 		/*a.forEach(value, index, ar)
+ 		{
+ 			var fileName = $('input[type=file]').val();
+			$("#filesUploaded").innerHTML += "a(href=\"#\", class=\"list-group-item active\") " + fileName;
+		}*/
+		for (var i = 0; i < e.files.length; ++i) {
+		  var name = e.files.item(i).name;
+		  alert("here is a file name: " + name);
+		}
+	});
+
 });
